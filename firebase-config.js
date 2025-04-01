20e4973788300136f115c0fb870fa36c27440997
Arcani-Dominus <arcanidominus@gmail.com>
2025-04-01 06:22:16 +0530

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -4,12 +4,12 @@ import { getFirestore } from "https://www.gstatic.com/firebasejs/10.5.2/firebase
 
 // ✅ Firebase configuration (Use your existing credentials)
 const firebaseConfig = {
-  apiKey: "AIzaSyChs_NAolpqRZ-dV22bZ5KXhqXa5XuNJTI",
-  authDomain: "orbis-arcana.firebaseapp.com",
-  projectId: "orbis-arcana",
-  storageBucket: "orbis-arcana.appspot.com", // Fixed incorrect domain
-  messagingSenderId: "474107878031",
-  appId: "1:474107878031:web:0869ada48ff6a446356efa"
+  apiKey: "AIzaSyC1T0aeLmHt3cEppYkSFJijsgAbKWdq3ks",
+  authDomain: "orbis-arcana-offline.firebaseapp.com",
+  projectId: "orbis-arcana-offline",
+  storageBucket: "orbis-arcana-offline.firebasestorage.app",
+  messagingSenderId: "125346575338",
+  appId: "1:125346575338:web:b1b82b69e71f35b146b893"
 };
 
 // ✅ Initialize Firebase
@@ -18,3 +18,5 @@ const db = getFirestore(app); // ✅ Initialize Firestore
 
 // ✅ Export Firestore database
 export { db };
+
+
